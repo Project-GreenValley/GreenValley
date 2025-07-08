d33602559a5c07fbe26da8d7e8d8a1b0fd585790
VaseanAnnin <vaseanannin@gmail.com>
2025-07-08 04:59:10 -0400

    successfully integrated prisma and improved home page styling

--- greenvalley/app/components/Navbar.tsx
+++ greenvalley/app/components/Navbar.tsx
@@ -2,15 +2,15 @@
 
 export default function Navbar() {
   return (
-    <nav className='flex items-center justify-between p-4 bg-gray-800 text-white w-full'>
-      <div className='hidden md:block text-2xl font-bold'>
-        <h1>GreenValley</h1>
+    <nav className='flex items-center justify-between p-4 text-[#1F2933] w-full'>
+      <div className='md:block text-2xl font-bold'>
+        <h1>GREENVALLEY</h1>
       </div>
       <div className='flex w-full md:w-auto justify-between md:justify-end items-center gap-0 md:gap-4'>
-        <button className='bg-green-500 text-white px-4 py-2 rounded'>
+        <button className=' x-4 p-2 rounded border-2 hover:text-[#A4DE02]'>
           Start a Campaign
         </button>
-        <button className='bg-gray-700 text-white px-4 py-2 rounded'>
+        <button className='  px-4 py-2 rounded hover:text-[#A4DE02]'>
           Login/Signup
         </button>
       </div>
