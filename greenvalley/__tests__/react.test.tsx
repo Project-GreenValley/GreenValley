e413f55dcc087a3e8ba291f9ccee7008c2abbdd3
VaseanAnnin <vaseanannin@gmail.com>
2025-07-08 03:31:41 -0400

    started incorporating prisma into the application to make SQL connections easier

--- greenvalley/__tests__/react.test.tsx
+++ greenvalley/__tests__/react.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Navbar from '../src/app/components/Navbar';
+import Navbar from '../app/app/components/Navbar';
 //describe is one of the ways in jest to start a test
 
 const resizeWindow = (x: number, y: number) => {
